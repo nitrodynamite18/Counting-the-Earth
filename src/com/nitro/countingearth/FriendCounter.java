75de24a6453c823103fc43089e92413ddac2eb95
nitrodynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2022-10-20 23:43:48 -0400

    Now works as intended.

--- src/com/nitro/countingearth/FriendCounter.java
+++ src/com/nitro/countingearth/FriendCounter.java
@@ -1,7 +1,23 @@
 package com.nitro.countingearth;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class FriendCounter {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("What are you trying to count to? Enter a whole number: ");
+        BigInteger countingTo = input.nextBigInteger();
+
+        String countingToBinary = countingTo.toString(2);
+
+        long numberOfBinaryDigits = countingToBinary.length();
+        // Creates a variable of type long and assigns it the value of the length of the String countingToBinary divided by 5, rounded up to the nearest whole number because we are assuming that each hand has 5 fingers.
+        long numberOfHands = (long) Math.ceil(numberOfBinaryDigits / 5.0);
+        // Creates a variable of type long and assigns it the value of the variable numberOfHands divided by 2, rounded up to the nearest whole number because we are assuming that each person has 2 hands.
+        long numberOfFriends = (long) Math.ceil(numberOfHands / 2.0);
+
+        System.out.println("You need " + numberOfFriends + " friends counting on their hands, using " + numberOfBinaryDigits + " fingers on " + numberOfHands + " hands, to count to " + countingTo + " in binary.");
     }
 }
